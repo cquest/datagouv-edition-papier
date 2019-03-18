fbb851db205167a6ae208220c29845a163a39db2
cquest <cquest@openstreetmap.fr>
2019-03-18 09:19:20 +0100

    ajout logos producteurs

--- make_latex.py
+++ make_latex.py
@@ -5,10 +5,13 @@ import sys
 import sqlite3
 import re
 import subprocess
+import os.path
 
 import pypandoc
 import psycopg2
 import psycopg2.extras
+import requests
+from PIL import Image
 
 def md2tex_old(md):
     # guillemets
@@ -46,6 +49,124 @@ def md2tex(md):
     md = pypandoc.convert_text(md, 'tex', format='md').replace('\\tightlist','')
     return md
 
+
+def data2tex(datagouv):
+  org = ''
+  for data in datagouv:
+    if org != data['name']:
+        # changement d'organisation
+        org = data['name']
+        logo = None
+        if data['logo'] != '':
+            logo = data['logo'].replace('https://static.data.gouv.fr/avatars/','').replace('/','_')
+            logo = 'images/orga/'+ logo
+            if not os.path.isfile(logo):
+                r = requests.get(data['logo'])
+                print('GET',data['logo'],logo)
+                if r.status_code == 200:
+                    if logo:
+                        with open(logo, 'wb') as fd:
+                            for chunk in r.iter_content(chunk_size=1024):
+                                fd.write(chunk)
+                        if r.headers['content-type'] == 'image/gif':
+                            gif = Image.open(logo)
+                            logo = logo.replace('.gif','.png')
+                            gif.save(logo)
+                else:
+                    logo = None
+            else:
+                logo = logo.replace('.gif','.png')
+
+        if logo and data['description_org'] != '':
+            out.write(crlf+"""
+\\clearpage
+\\section{%(name)s}
+\\fbox{
+  \\begin{minipage}{0.95\\textwidth}
+    \\begin{wrapfigure}{r}{%(width)s}
+      \\centering
+      \\includegraphics[width=%(width)s]{%(logo)s}
+    \\end{wrapfigure}
+  %(description)s
+  \\end{minipage}
+}
+
+""" % {'name': md2tex(data['name']),
+       'width': qrcode_width,
+       'description': md2tex(data['description_org']),
+       'logo': logo,
+       'qr': 'https://data.gouv.fr/organization/'+data['organization_id']}
+            )
+        elif data['description_org'] != '':
+            out.write(crlf+"""
+\\clearpage
+\\section{%(name)s}
+\\fbox{
+  \\begin{minipage}{0.95\\textwidth}
+  %(description)s
+  \\end{minipage}
+}
+\\vspace{1cm}
+
+""" % {'name': md2tex(data['name']),
+       'width': '3cm',
+       'description': md2tex(data['description_org']),
+       'qr': 'https://data.gouv.fr/organization/'+data['organization_id']}
+            )
+
+
+    # génération du QRcode
+    #out.write(crlf+'\\pagebreak'+crlf)
+    if data['id']=='z58301bbb88ee3809c3c65bb3':
+        print(data)
+        m = data['description']
+        m = re.sub(re.compile('!(\[|\().*?\)', re.MULTILINE), r'', m.replace('\n]',']'))
+        print(m)
+        exit()
+    out.write('\\addcontentsline{toc}{subsection}{%s}' % (md2tex(data['title'].capitalize()), ) + crlf)
+    out.write('\\subsection*{%s}' % (md2tex(data['title'].capitalize(), )) + crlf+crlf)
+    if data['tags']:
+        for mot in data['tags'].split(','):
+            out.write('\\index{%s}' % (mot.replace('-','!'),))
+    out.write(crlf+'\\noindent'+crlf)
+    out.write(md2tex(data['description'])+crlf)
+    out.write("""
+\\par
+\\noindent
+\\hrulefill
+\\begin{samepage}
+  \\begin{wrapfigure}{r}{%(width)s}
+    \\centering
+    \\qrcode[nolink]{%(url)s}
+  \\end{wrapfigure}
+""" % {'width': qrcode_width, 'url': "https://data.gouv.fr/dataset/"+data['id']} + crlf)
+    out.write('\\noindent'+crlf)
+    out.write('Licence : \\textbf{%s}\\par' % (md2tex(data['license'] if data['license'] != '' else 'non renseignée'),)+crlf+crlf)
+
+    out.write('\\noindent'+crlf)
+    out.write('Créé le : %s\\par' % (str(data['created_at'])[:10], )+crlf+crlf)
+    out.write('\\noindent'+crlf)
+    out.write('Modifié le : %s\\par' % (str(data['last_modified'])[:10], )+crlf+crlf)
+
+    if data['temporal_coverage.start'] and data['temporal_coverage.end']:
+        out.write('\\noindent'+crlf)
+        out.write('De %s à %s\\par' % (str(data['temporal_coverage.start'])[:10], str(data['temporal_coverage.end'])[:10])+crlf+crlf)
+    if data['spatial.granularity'] in gran:
+        out.write('\\noindent'+crlf)
+        out.write('Granularité : %s\\par' % (gran[data['spatial.granularity']], )+crlf+crlf)
+    if data['frequency'] in freq:
+        out.write('\\noindent'+crlf)
+        out.write('Mise à jour : %s\\par' % (freq[data['frequency']], )+crlf+crlf)
+    out.write('\\noindent'+crlf)
+    out.write('Mots-clé : \\emph{%s}\\par' % (md2tex(data['tags'] if data['tags'] and data['tags'] != '' else 'aucun').replace(',',', ',))+crlf+crlf)
+    out.write('\\noindent'+crlf)
+    out.write('Permalien : \\url{%s}\\par' % (site_url+'dataset/'+data['id'], )+crlf)
+    out.write("""
+\\end{samepage}
+\\vspace{0.5cm}
+""")
+
+
 out = open('datagouv.tex','w')
 crlf = '\x0d\x0a'
 out.write("""\\documentclass[a4paper, 12pt]{book}
@@ -61,7 +182,8 @@ out.write("""\\documentclass[a4paper, 12pt]{book}
 \\usepackage{imakeidx}
 \\usepackage{wrapfig}
 \\usepackage{graphicx}
-\\graphicspath{ {./images/} }
+
+\\usepackage{qrcode}
 
 \\makeindex[intoc]
 
@@ -100,26 +222,16 @@ Ce chapitre liste les jeux de données de référence définis par le \\href{htt
 \\noindent
 Voir aussi \\url{https://www.data.gouv.fr/fr/reference}
 
-\\pagebreak
+\\clearpage
 
 \\chapter{Données des services publics certifiés}
 
 Ce chapitre liste les jeux de données produits par les services publics répertoriés sur le portail national \\href{https://data.gouv.fr/}{data.gouv.fr}.
 
-\\pagebreak
+\\clearpage
 
 """+crlf)
 
-if len(sys.argv)>1:
-    csv.field_size_limit(250000)
-    datagouv = csv.DictReader(open(sys.argv[1], 'r'))
-else:
-    pg = psycopg2.connect("dbname=datagouv")
-    db = pg.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    db.execute("""select * from datagouv_data where badges ~ 'certified' order by name,title limit 100""")
-    datagouv = db.fetchall()
-
-n=0
 
 site_url = 'https://data.gouv.fr/'
 qrcode_width = '2.5cm'
@@ -143,86 +255,39 @@ gran = {'fr:commune': 'à la commune',
         'fr:canton': 'au canton',
         'fr:iris': "à l'IRIS INSEE",
         'fr:collectivite': 'à la collectivité' }
-
-org = ''
-
 crlf='\x0d\x0a'
-for data in datagouv:
-    if org != data['name']:
-        # changement d'organisation
-        org = data['name']
-        out.write(crlf+"""
-\\pagebreak
-\\section{%s}
-\\fbox{\\begin{minipage}{0.95\\textwidth}%s\\end{minipage}}
 
-""" % (md2tex(data['name']), md2tex(data['description_org'])))
 
-    # génération du QRcode
-    qr = 'images/dataset/d_%s.png' % data['id']
-    subprocess.call('if [ ! -f %s ]; then qr --factory=pymaging "https://www.data.gouv.fr/dataset/%s" > %s ; fi'
-        % (qr, data['id'], qr), shell=True)
 
-    #out.write(crlf+'\\pagebreak'+crlf)
-    if data['id']=='z58301bbb88ee3809c3c65bb3':
-        print(data)
-        m = data['description']
-        m = re.sub(re.compile('!(\[|\().*?\)', re.MULTILINE), r'', m.replace('\n]',']'))
-        print(m)
-        exit()
-    out.write('\\addcontentsline{toc}{subsection}{%s}' % (md2tex(data['title'].capitalize()), ) + crlf)
-    out.write('\\subsection*{%s}' % (md2tex(data['title'].capitalize(), )) + crlf+crlf)
-    if data['tags']:
-        for mot in data['tags'].split(','):
-            out.write('\\index{%s}' % (mot.replace('-','!'),))
-    out.write(crlf+'\\noindent'+crlf)
-    out.write(md2tex(data['description'])+crlf)
-    out.write("""
-\\par
-\\noindent
-\\hrulefill
-\\begin{samepage}
-\\noindent
-\\begin{wrapfigure}{r}{%s}
-  \\centering
-  \\includegraphics[width=%s]{%s}
-\\end{wrapfigure}
-""" % (qrcode_width, qrcode_width, qr) + crlf)
-    out.write('\\noindent'+crlf)
-    out.write('Licence : \\textbf{%s}\\par' % (md2tex(data['license'] if data['license'] != '' else 'non renseignée'),)+crlf+crlf)
+if len(sys.argv)>1:
+    csv.field_size_limit(250000)
+    datagouv = csv.DictReader(open(sys.argv[1], 'r'))
+else:
+    pg = psycopg2.connect("dbname=datagouv")
+    db = pg.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    db.execute("""select * from datagouv_data where badges ~ 'certified' order by name,title limit 100""")
+    datagouv = db.fetchall()
 
-    out.write('\\noindent'+crlf)
-    out.write('Créé le : %s\\par' % (str(data['created_at'])[:10], )+crlf+crlf)
-    out.write('\\noindent'+crlf)
-    out.write('Modifié le : %s\\par' % (str(data['last_modified'])[:10], )+crlf+crlf)
-
-    if str(data['temporal_coverage.start']) != '' and str(data['temporal_coverage.end']) != '':
-        out.write('\\noindent'+crlf)
-        out.write('De %s à %s\\par' % (str(data['temporal_coverage.start'])[:10], str(data['temporal_coverage.end'])[:10])+crlf+crlf)
-    if data['spatial.granularity'] in gran:
-        out.write('\\noindent'+crlf)
-        out.write('Granularité : %s\\par' % (gran[data['spatial.granularity']], )+crlf+crlf)
-    if data['frequency'] in freq:
-        out.write('\\noindent'+crlf)
-        out.write('Mise à jour : %s\\par' % (freq[data['frequency']], )+crlf+crlf)
-    out.write('\\noindent'+crlf)
-    out.write('Mots-clé : \\emph{%s}\\par' % (md2tex(data['tags'] if data['tags'] and data['tags'] != '' else 'aucun').replace(',',', ',))+crlf+crlf)
-    out.write('\\noindent'+crlf)
-    out.write('Permalien : \\url{%s}\\par' % (site_url+'dataset/'+data['id'], )+crlf)
-    out.write("""
-\\end{samepage}
-\\vspace{0.5cm}
-""")
-
+data2tex(datagouv)
 
 out.write("""
 
-\\pagebreak
+\\clearpage
 
 \\chapter{Autres jeux de données}
 
 Ce chapitre liste les autres jeux de données disponibles sur \\emph{data.gouv.fr}.
+""")
 
+if len(sys.argv)<2:
+    pg = psycopg2.connect("dbname=datagouv")
+    db = pg.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    db.execute("""select * from datagouv_data where badges !~ 'certified' order by name,title limit 100""")
+    datagouv = db.fetchall()
+
+data2tex(datagouv)
+
+out.write("""
 \\backmatter
 
 \\printindex
