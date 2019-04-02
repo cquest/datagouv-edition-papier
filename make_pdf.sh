45305a0bbdabdcb034c159a6e69349dc12245002
cquest <cquest@openstreetmap.fr>
2019-04-02 09:16:28 +0200

    séparation header/body/footer

--- make_pdf.sh
+++ make_pdf.sh
@@ -1,5 +1,5 @@
-rm -f datagouv.* \
-&& ./make_latex.py \
-&& xelatex datagouv.tex > log \
-&& xelatex datagouv.tex > log
+./make_latex.py \
+&& cat includes/header.tex datagouv.tex includes/footer.tex > final.tex \
+&& xelatex final.tex  \
+&& xelatex final.tex 
 
